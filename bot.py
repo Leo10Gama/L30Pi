361b7b8e0844b4be8f8617c0b3b0afe1067408f2
Leonardo Gama <leo10gama@gmail.com>
2020-06-25 11:22:51 -0400

    Added math command

--- bot.py
+++ bot.py
@@ -9,7 +9,8 @@ PREFIX = "p."
 command_help = {
     "ping": "`p.ping`\nA simple debug command where I respond with 'pong'",
     "help": "`p.help`\nList all available commands\n`p.help [command]`\nGet further details on a specific command",
-    "palindrome": "`p.palindrome [word]`\nFigure out if a word is spelt the same forwards and backwards"
+    "palindrome": "`p.palindrome [word]`\nFigure out if a word is spelt the same forwards and backwards",
+    "math": "`p.math [number] [operation] [number]`\nPerform a mathematical operation using either {+, -, *, /, %, \\}"
 }
 command_list = list(command_help.keys())
 
@@ -47,6 +48,35 @@ async def on_message(message):
                 await message.channel.send("This word **is** a palindrome")
             else:
                 await message.channel.send("This word **is not** a palindrome")
+        # Math command
+        elif command[:4] == command_list[3]:
+            maths = command[4:].strip().split()
+            if len(maths) == 3:
+                try:
+                    if maths[1] == "+":
+                        await message.channel.send(maths[0] + " " + maths[1] + " " + maths[2] +
+                              " = " + str(float(maths[0]) + float(maths[2])))
+                    elif maths[1] == "-":
+                        await message.channel.send(maths[0] + " " + maths[1] + " " + maths[2] +
+                              " = " + str(float(maths[0]) - float(maths[2])))
+                    elif maths[1] == "*":
+                        await message.channel.send(maths[0] + " " + maths[1] + " " + maths[2] +
+                              " = " + str(float(maths[0]) * float(maths[2])))
+                    elif maths[1] == "/":
+                        await message.channel.send(maths[0] + " " + maths[1] + " " + maths[2] +
+                              " = " + str(float(maths[0]) / float(maths[2])))
+                    elif maths[1] == "%":
+                        await message.channel.send(maths[0] + " " + maths[1] + " " + maths[2] +
+                              " = " + str(float(maths[0]) % float(maths[2])))
+                    elif maths[1] == "\\":
+                        await message.channel.send(maths[0] + " " + maths[1] + " " + maths[2] +
+                              " = " + str(float(maths[0]) // float(maths[2])))
+                    else:
+                        await message.channel.send("Invalid input")
+                except:
+                    await message.channel.send("Invalid input")
+            else:
+                await message.channel.send("Invalid operation")
         #TODO: Add more commands here
         else:
             await message.channel.send("Command not found. Try typing `p.help` to see a list of all commands")
