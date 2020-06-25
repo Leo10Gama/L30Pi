2792b65d623d38ceeebb2b1d395bc868ae57a893
Leonardo Gama <leo10gama@gmail.com>
2020-06-25 11:10:17 -0400

    Refined commands to make it easier to add more

--- bot.py
+++ bot.py
@@ -6,6 +6,11 @@ load_dotenv()
 TOKEN = os.getenv("DISCORD_TOKEN")
 client = discord.Client()
 PREFIX = "p."
+command_help = {
+    "ping": "`p.ping`\nA simple debug command where I respond with 'pong'",
+    "help": "`p.help`\nList all available commands\n`p.help [command]`\nGet further details on a specific command"
+}
+command_list = list(command_help.keys())
 
 # This event will execute when the bot goes online
 @client.event
@@ -21,8 +26,18 @@ async def on_message(message):
 
     # Determine if the message sent is something the bot should respond to
     if message.content.startswith(PREFIX):
-        commands = message.content[2:]
-        if commands == "ping":
+        command = message.content[2:].lower().strip()
+        # Ping command
+        if command[:4] == command_list[0]:
             await message.channel.send('Pong!')
+        # Help command
+        elif command[:4] == command_list[1]:
+            command = command[4:].strip()
+            # Get information on a specific command
+            if command in command_help:
+                await message.channel.send(command_help[command])
+            # View list of all commands
+            else:
+                await message.channel.send("Here are some commands I am capable of:\n`" + "`\n`".join(command_list) + "`")
 
 client.run(TOKEN)
