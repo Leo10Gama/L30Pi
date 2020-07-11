fc53277e599f918bde9ed5a98c5eed6d410c6e34
Leonardo Gama <leo10gama@gmail.com>
2020-07-11 10:47:01 -0400

    Added more cats (as god intended)

--- cat.py
+++ cat.py
@@ -4,7 +4,11 @@ class Cat:
     def __init__(self, name, num_of_pics):
         self.name, self.num_of_pics = name, num_of_pics
 
-CATS = {"ollie": Cat("ollie", 23), "mushu": Cat("mushu", 8)}
+CATS = {"ollie": Cat("ollie", 23), 
+    "mushu": Cat("mushu", 8), 
+    "achilles": Cat("achilles", 5), 
+    "luna": Cat("luna", 8),
+    "winter": Cat("winter", 6)}
 
 # Returns an array of length 2 with the cat's name and an image of them
 def get_cat_image(cat_name=""):
